76a7ff746dddd57d2b8fa0bdf9a86b0f87a78569
CTWills <cameron.wills.1998@gmail.com>
2023-07-18 12:21:14 -0600

    created get endpoint for all users

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const knex = require('knex')(require('./knexfile.js').development)
 const port = 8080;
 const app = express();
 
 app.use(cors());
 app.use(express.json())
 
+
+//all CRUD endpoints for users
+app.get('/users', (req, res) => {
+    knex.select('*')
+        .from('users')
+        .then(data => res.status(200).json(data))
+})
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
