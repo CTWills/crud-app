84799a9fea40b08bf03e0b82741728eb10462ecc
CTWills <cameron.wills.1998@gmail.com>
2023-07-18 15:00:38 -0600

    created login ui

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,32 @@
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import './Login.css';
+
 const Login = () => {
 
     return (
-        <>This is the login screen</>
+            <form className='login-form' autoComplete='off'>
+                    <TextField
+                        required
+                        id="username"
+                        label="Username"
+                        variant="outlined"
+                        onChange={(e) => console.log(e.target.value)}
+                    />
+                    <TextField
+                        required
+                        id="password"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                        variant="outlined"
+                        onChange={(e) => console.log(e.target.value)}
+                    />
+                <div className='login-buttons'>
+                    <Button id='login-button' variant='contained' type='submit'>Login</Button>
+                    <Button variant='contained' type='button'>Create</Button>
+                </div>
+            </form>
     )
 }
 
