048c02bb3f33727244a003ba3d1bfa46524edae9
CTWills <cameron.wills.1998@gmail.com>
2023-07-20 14:40:01 -0600

    header now displays who you are logged in as

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Login from './components/Login';
 import Create from './components/Create';
 import { useNavigate } from 'react-router-dom';
 import Home from './components/Home';
-import { useState, createContext} from 'react';
+import { useState, createContext, useEffect} from 'react';
 import AddItem from './components/AddItem';
 import Item from './components/Item';
 import GuestHome from './components/GuestHome';
@@ -14,6 +14,18 @@ export const UserContext = createContext();
 const App = () => {
   const navigate = useNavigate();
   const [ user, setUser ] = useState([]);
+  const [ username, setUsername ] = useState();
+
+  useEffect(() => {
+    if (user.id) {
+        localStorage.setItem('username', `${user.username}`)
+        let username = localStorage.getItem('username')
+        setUsername(username)
+    } else {
+        let username1 = localStorage.getItem('username')
+        setUsername(username1)
+    }
+}, [user])
   
   return (
     <UserContext.Provider value={ {user, setUser} } >
@@ -24,7 +36,10 @@ const App = () => {
                   navigate('/homepage')
                 }
               }}>The Inventory</h1>
-              <div id='header-logout' onClick={() => {navigate('/'); localStorage.setItem('id', '')}}>Logout</div>
+              <div className='header-logout'>
+                <div id='header-logout' onClick={() => {navigate('/'); localStorage.setItem('id', ''); localStorage.setItem('username', ''); window.location.reload()}}>Logout</div>
+                {username ? <div>Logged in as: {username}</div> : <></>}
+              </div>
           </div>
           <Routes>
             <Route path='/' element={<Login />}/>
